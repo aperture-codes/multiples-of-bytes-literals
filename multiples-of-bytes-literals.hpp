30fc15ff585b291783d98283c36d3d277fce702f
Jakub Świerk <jakub@swierk.me>
2017-06-04 20:48:06 +0200

    Fixed casting from singed int to uint in power_of_1024.

--- multiples-of-bytes-literals.hpp
+++ multiples-of-bytes-literals.hpp
@@ -1,36 +1,34 @@
 #pragma once
 
+#include <cassert>
+#include <climits>
 #include <cstddef>
-#include <cmath>
 
 namespace multiples_of_bytes_literals {
 
-namespace details {
-    constexpr std::size_t power_of_1024(unsigned char exponent) {
-        return 1 << 10*exponent;
+    namespace details {
+        constexpr std::size_t power_of_1024(unsigned char exponent) {
+            return std::size_t(1) << 10 * exponent;
+        }
+
+        constexpr std::size_t bytes(unsigned long long value, unsigned char exponent) {
+            return value * power_of_1024(exponent);
+        }
+
+        constexpr std::size_t bytes(long double value, unsigned char exponent) {
+            return value * power_of_1024(exponent);
+        }
     }
 
-    constexpr std::size_t bytes(unsigned long long value,unsigned char exponent) {
-        return value*power_of_1024(exponent);
-    }
+#define define_literal(name, exponent)                                                                             \
+    constexpr std::size_t operator"" _##name(unsigned long long value) { return details::bytes(value, exponent); } \
+    constexpr std::size_t operator"" _##name(long double value) { return details::bytes(value, exponent); }
 
-    constexpr std::size_t bytes(long double value,unsigned char exponent) {
-        return value*power_of_1024(exponent);
-    }
-}
-
-using namespace details;
-
-#define define_literal(name, exponent) \
-    constexpr std::size_t operator"" _##name(unsigned long long value) { return bytes(value, exponent); }\
-    constexpr std::size_t operator"" _##name(long double value) { return bytes(value, exponent); }
-
-define_literal(B, 0)
-define_literal(KiB, 1)
-define_literal(MiB, 2)
-define_literal(GiB, 3)
-define_literal(TiB, 4)
+    define_literal(B, 0)
+    define_literal(KiB, 1)
+    define_literal(MiB, 2)
+    define_literal(GiB, 3)
+    define_literal(TiB, 4)
 
 #undef define_literal
-
 }
