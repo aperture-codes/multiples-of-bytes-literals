52c161c8ec772850e60bdb4289d7e0ca0c7299ce
Jakub Świerk <jakub@swierk.me>
2017-09-09 22:07:19 +0200

    Formatted tests code.

--- tests/doubles_test.cpp
+++ tests/doubles_test.cpp
@@ -1,12 +1,12 @@
+#include "multiples-of-bytes-literals.hpp"
 #include <cassert>
 #include <cstdlib>
-#include "multiples-of-bytes-literals.hpp"
 
 using namespace multiples_of_bytes_literals;
 
-int main(int argc, char* argv[]) {
-    assert((1_MiB+0.5_MiB) == 1.5_MiB);
-    assert((1_MiB+0_MiB) == 1_MiB);
-    assert((1_MiB*24) == 24_MiB);
+int main(int argc, char *argv[]) {
+    assert((1_MiB * 24) == 24_MiB);
+    assert((1_MiB + 0_MiB) == 1_MiB);
+    assert((1_MiB + 0.5_MiB) == 1.5_MiB);
     return EXIT_SUCCESS;
 }
--- tests/size_test.cpp
+++ tests/size_test.cpp
@@ -1,14 +1,14 @@
+#include "multiples-of-bytes-literals.hpp"
 #include <cassert>
 #include <cstdlib>
-#include "multiples-of-bytes-literals.hpp"
 
 using namespace multiples_of_bytes_literals;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     assert((1L) == 1_B);
-    assert((1L*1024) == 1_KiB);
-    assert((1L*1024*1024) == 1_MiB);
-    assert((1L*1024*1024*1024) == 1_GiB);
-    assert((1L*1024*1024*1024*1024) ==  1_TiB);
-	return EXIT_SUCCESS;
+    assert((1L * 1024) == 1_KiB);
+    assert((1L * 1024 * 1024) == 1_MiB);
+    assert((1L * 1024 * 1024 * 1024) == 1_GiB);
+    assert((1L * 1024 * 1024 * 1024 * 1024) == 1_TiB);
+    return EXIT_SUCCESS;
 }
